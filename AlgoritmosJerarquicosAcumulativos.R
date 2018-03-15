bed3be2138e6d881861be96df27f33f5c4a4a08c
CIEMA-ActuariaA <ciema.actuaria@anahuac.mx>
2018-03-14 20:27:09 -0600

    Primer Script de Analisis Jerarquico

---
+++ AlgoritmosJerarquicosAcumulativos.R
@@ -0,0 +1,22 @@
+#### Proyecto de Investigacion: Analisis de Conglomerados
+## Métodos de Análisis de Conglomerados
+# Luis Gerardo Calderón Contreras
+#20180314
+
+
+#Descripcion: Script ilustrativo de Algoritmos Jerarquicos Acumulativos.
+#Objetivos:
+  # Aplicar el Análisis de Conglomerados en R
+  # Interpretar el dendograma resultante del análisis
+  # Clasificar Grupos a partir de distancias o medidas de similitud
+
+#Paquetes Necesarios
+library(tidyverse)  # Manipulación de datos.
+library(cluster)    # Algoritmos de conglomerados.
+library(factoextra) # Visualizar Conglomerados.
+library(dendextend) # Comparar dendogramas.
+
+
+#Analisis Exploratorio
+
+sum()
