ad3f0984c57f862b2ce663ac69a11fb9f261ef9c
luisgcalderon <luis.calderon@anahuac.mx>
2018-03-16 11:04:02 -0600

    N20180316

--- AlgoritmosJerarquicosAcumulativos.R
+++ AlgoritmosJerarquicosAcumulativos.R
@@ -1,22 +1,32 @@
 #### Proyecto de Investigacion: Analisis de Conglomerados
-## Métodos de Análisis de Conglomerados
-# Luis Gerardo Calderón Contreras
+## Metodos de Analisis de Conglomerados
+# Luis Gerardo Calderon Contreras
 #20180314
 
 
 #Descripcion: Script ilustrativo de Algoritmos Jerarquicos Acumulativos.
 #Objetivos:
-  # Aplicar el Análisis de Conglomerados en R
-  # Interpretar el dendograma resultante del análisis
+  # Aplicar el Analisis de Conglomerados en R
+  # Interpretar el dendograma resultante del analisis
   # Clasificar Grupos a partir de distancias o medidas de similitud
 
 #Paquetes Necesarios
-library(tidyverse)  # Manipulación de datos.
+install.packages(c("tidyverse","cluster","factoextra","dendextend"))
+library(tidyverse)  # Manipulacion de datos.
+install.packages("cluster")
 library(cluster)    # Algoritmos de conglomerados.
 library(factoextra) # Visualizar Conglomerados.
 library(dendextend) # Comparar dendogramas.
 
-
+X<-faithful
 #Analisis Exploratorio
 
-sum()
+X<-na.omit(X)
+X<-scale(X)
+
+plot(X)
+#Dissimilarity Matrix
+d<-dist(X,method="manhattan")
+hc1<-hclust(d,method = "complete")
+plot(hc1, cex=0.6,hang=-1,main = "Dendograma OldFaithFull")
+?dist
