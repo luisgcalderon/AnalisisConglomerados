189657a7d9bcfc77096b1d085bdce92e3cb4bda4
luisgcalderon <luis.calderon@anahuac.mx>
2018-03-29 09:11:14 -0600

    Plot & Adjustments

--- EM Algorithm/EM_OldFaithFull v2 20180202.R
+++ EM Algorithm/EM_OldFaithFull v2 20180202.R
@@ -23,9 +23,9 @@ ggplot(faithful,aes(waiting,eruptions))+
   geom_point()
 View(faithful)
 
-ggplot(faithful,aes(waiting,1))+
+ggplot(faithful,aes(waiting,0))+
   geom_point()+
-  stat_function(fun = dnorm,n = 101, args = list(mean=mean(x$eruptions),sd=sd(x$eruptions)))
+  stat_function(fun = dnorm,n = 100, args = list(mean=mean(x$waiting),sd=sd(x$waiting)))
 
 # Data to Model ----
 x<-faithful$waiting
@@ -36,8 +36,23 @@ g<-(length(p)) #Componentes del Modelo
 mu <- c(52,82); sig<- c(16,16) # Parametros Mu y Sigma respectivamente
 psi<-data.frame(p,mu,sig)
 
+# Plot EM ----
+rainbowcols <- rainbow(g)
+d<-list()
+for (i in 1:g) {
+  d<-c(stat_function(fun = dnorm,n = 100, args = list(mean=psi[i,"mu"],
+                                                      sd=sqrt(psi[i,"sig"])),
+                     color=rainbowcols[i]),d)
+}
+
+plot1<-ggplot(data.frame(i<-c(1:length(x)),x),aes(x,-0.005))+
+  geom_point() + d + theme(
+    panel.background = element_rect(fill = "white"))+ylim(-0.01,0.1) + ylab("")
+plot(plot1)
+
+
 #relativo, angulo, verosi
-psi.rel<-EMAlgorithm1d(dato = x,g = g,psi = psi,metodo = "rel",difmin = 0.0001)
+psi.rel<-EMAlgorithm1d(dato = x,g = g,psi = psi,metodo = "relativo",difmin = 0.0001)
 psi.ang<-EMAlgorithm1d(dato = x,g = g,psi = psi,metodo = "angulo",difmin = 0.0001)
 psi.ver<-EMAlgorithm1d(dato = x,g = g,psi = psi,metodo = "verosi",difmin = 0.0001)
 
