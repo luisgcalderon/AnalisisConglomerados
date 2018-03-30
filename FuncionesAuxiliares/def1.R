29c2481061a41af861d59bdd8dc76c2f4b4f5b27
luisgcalderon <luis.calderon@anahuac.mx>
2018-03-29 19:59:22 -0600

    Construcion de EM MV Paso M

--- FuncionesAuxiliares/def1.R
+++ FuncionesAuxiliares/def1.R
@@ -20,3 +20,37 @@ q<-function(c,me,des,colr){stat_function(fun = dnorm,n = 101,
                                                args = list(mean=des,
                                                            sd=des,col=colr))
                                                }
+
+
+?mvt
+??mvt
+#Idea particionar el dataframe como sumas de riemman
+library(mvtnorm)
+n<-2
+dmvnorm(x = x,mean = psi[,],sigma = )
+psi[,"mu.mu1"]
+EMStepsMV<-function(x, g, psi, q){
+  m<-dim(x)[2] #dimension de x
+  n<-dim(x)[1] #numero de observaciones
+  psi.t<-psi
+  for (i in 1:(g-1)) {
+    t<-numeric(n)
+    t<-psi[i,"p"]*dmvnorm(x = x,mean = psi[1:m,1+i],sigma = 
+                            matrix(data = c(psi[,g+2*i],psi[,g+2*i+1])))
+    for (j in 1:n) {
+      a<-numeric(n)
+      for (w in 1:g) {
+        a<-a+psi[w,1]*dmvnorm(x = x[j,],mean = psi[1:m,1+w],sigma = 
+                                matrix(data = c(psi[,g+2*w],psi[,g+2*w+1]),nrow = 2))
+        
+      }
+      t[j]<-t[j]/a
+    }
+    psi.t[i,"p"]<-sum(t)/n
+    for (j in 1:m) {
+      psi.t[j,1+i]<-sum(t%*%x[,j])/sum(t)
+      
+    }
+    #estimacion de mu
+  }
+}
