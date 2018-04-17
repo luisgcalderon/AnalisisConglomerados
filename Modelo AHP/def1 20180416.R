a34c93b9a6c611c2104a036899970feca61b15df
luisgcalderon <luis.calderon@anahuac.mx>
2018-04-16 20:30:08 -0500

    Inicio de Definicion

---
+++ Modelo AHP/def1 20180416.R
@@ -0,0 +1,9 @@
+#Universidad Anahuac Mexico
+#Proyecto de Investigacion
+#Analisis de Conglomerados
+#Luis Gerardo Calderon Contreras
+
+#Proposed Pairwise Comparison Matrix
+library(readr)
+ProposedPairwiseComparisonMatrix <- read_csv("Modelo AHP/ProposedPairwiseComparisonMatrix.csv")
+colnames(ProposedPairwiseComparisonMatrix)
