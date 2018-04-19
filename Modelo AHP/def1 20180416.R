2e926eddffaa78ea5a52edd6414dbf291973fed1
CIEMA-ActuariaA <ciema.actuaria@anahuac.mx>
2018-04-19 09:54:50 -0500

    Desarrollo

--- Modelo AHP/def1 20180416.R
+++ Modelo AHP/def1 20180416.R
@@ -20,6 +20,9 @@ apply(NMatrix,1,mean)
 
 #AHP Model Analysis----
 
+g<-2 # cantidad de cluster Inicial
+n<-4 # alternativas
+
 #Step1 Hierarchy Structure
 #Graficar la estructura del Analisis
 
@@ -31,21 +34,28 @@ apply(NMatrix,1,mean)
 #Estimar Parametros Iniciales
 
 ##Step2.2 Modelled with EM
-#Estimar los parametros 
+#Estimar los parametros finales
 
 #Inputs datos,psi, g, graf
-psi.2<-EMAlgorithmMV(dato = x,g = 2,psi = psi.2,metodo = "verosi",difmin = 0.001)
-psi.3<-EMAlgorithmMV(dato = x,g = 3,psi = psi.3,metodo = "verosi",difmin = 0.001)
-psi.4<-EMAlgorithmMV(dato = x,g = 4,psi = psi.4,metodo = "verosi",difmin = 0.001)
-psi.5<-EMAlgorithmMV(dato = x,g = 5,psi = psi.5,metodo = "verosi",difmin = 0.001)
+psi.2<-EMAlgorithmMV(dato = x,g = g,psi = psi.2,metodo = "verosi",difmin = 0.001)
+psi.3<-EMAlgorithmMV(dato = x,g = g+1,psi = psi.3,metodo = "verosi",difmin = 0.001)
+psi.4<-EMAlgorithmMV(dato = x,g = g+2,psi = psi.4,metodo = "verosi",difmin = 0.001)
+psi.5<-EMAlgorithmMV(dato = x,g = g+3,psi = psi.5,metodo = "verosi",difmin = 0.001)
 
 #Step3 Criterion Information Validation
 
-
+DeMatrix<-data.frame(matrix(data = c(IC(x,g,psi,metodo="ALL"),IC(x,g,psi,metodo="ALL"),IC(x,g,psi,metodo="ALL"),IC(x,g,psi,metodo="ALL"))
+                            ,nrow = n,ncol = 5,byrow = T)); colnames(DeMatrix)<-c("AIC","AWE","BIC","CLC","KIC");row.names(DeMatrix)<-as.character(g:(g+n-1))
 
 
 
 #Step4 Pairwise comparison Matrices for each Criterio
+#AIC
+
+#AWE
+#BIC
+#CLC
+#KIC
 
 #Step5 C-RIV 
 
