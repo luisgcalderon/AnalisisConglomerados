af70696fec071b745920bc62cea053d8f1172547
CIEMA-ActuariaA <ciema.actuaria@anahuac.mx>
2018-04-19 09:53:55 -0500

    Optimizacion

--- Criterios de Informacion/Definicion de Funciones.R
+++ Criterios de Informacion/Definicion de Funciones.R
@@ -25,15 +25,17 @@ IC<-function(x,g,psi,metodo){
     t<-t+psi$p[i]*dmvnorm(x = x,mean = unlist(psi$mu[i]),sigma = 
                                     matrix(data = unlist(psi$Sig[i]),nrow = m))
   }
-  if (metodo=="AIC"){
   FVer<-sum(log(t))
-  FVer<-(-2)*FVer+2*(length(unlist(psi)))
-  return(FVer)
-  } else if (metodo=="AWE"){
-    FVer<-sum(log(t))
-    t<-matrix(0,nrow=n,ncol = g)
+  if (metodo=="AIC"|metodo=="ALL"){
+  FVerAIC<-(-2)*FVer+2*(length(unlist(psi)))
+    if (metodo=="AIC"){
+      return(FVerAIC)
+    }
+  }
+  if (metodo=="AWE"|metodo=="ALL"){
+    mt<-matrix(0,nrow=n,ncol = g)
     for (i in 1:g) {
-      t[,i]<-psi$p[i]*dmvnorm(x = x,mean = unlist(psi$mu[i]),sigma = 
+      mt[,i]<-psi$p[i]*dmvnorm(x = x,mean = unlist(psi$mu[i]),sigma = 
                             matrix(data = unlist(psi$Sig[i]),nrow=m))
       for (j in 1:n) {
         a<-numeric(1)
@@ -42,52 +44,65 @@ IC<-function(x,g,psi,metodo){
                                   matrix(data = unlist(psi$Sig[w]),nrow = m))
           
         }
-        t[j,i]<-t[j,i]/a
+        mt[j,i]<-mt[j,i]/a
       }
     }
     ent<-numeric(1)
     for (w in 1:g){
-      ent<-ent+t[,w]%*%log(t[,w])
+      ent<-ent+mt[,w]%*%log(mt[,w])
     }
-  FVer<-(-2)*(FVer-as.numeric(ent))+2*length(unlist(psi))*(3/2+log(n))
-  return(FVer)
-  } else if (metodo=="BIC"){
-    FVer<-sum(log(t))
-    FVer<-(-2)*FVer+(length(unlist(psi))*log(n))
-    return(FVer)
-  } else if (metodo=="CLC"){
-    FVer<-sum(log(t))
-    t<-matrix(0,nrow=n,ncol = g)
-    for (i in 1:g) {
-      t[,i]<-psi$p[i]*dmvnorm(x = x,mean = unlist(psi$mu[i]),sigma = 
-                            matrix(data = unlist(psi$Sig[i]),nrow=m))
-      for (j in 1:n) {
-        a<-numeric(1)
-        for (w in 1:g) {
-          a<-a+psi$p[w]*dmvnorm(x = x[j,],mean = unlist(psi$mu[w]),sigma = 
-                                  matrix(data = unlist(psi$Sig[w]),nrow = m))
+  FVerAWE<-(-2)*(FVer-as.numeric(ent))+2*length(unlist(psi))*(3/2+log(n))
+    if (metodo=="AWE"){
+      return(FVerAWE)
+    }
+  }
+  if (metodo=="BIC"|metodo=="ALL"){
+    FVerBIC<-(-2)*FVer+(length(unlist(psi))*log(n))
+    if (metodo=="BIC"){
+      return(FVerBIC)
+    }
+  }
+  if (metodo=="CLC"|metodo=="ALL"){
+    if (metodo=="CLC"){
+      mt<-matrix(0,nrow=n,ncol = g)
+      for (i in 1:g) {
+        mt[,i]<-psi$p[i]*dmvnorm(x = x,mean = unlist(psi$mu[i]),sigma = 
+                                  matrix(data = unlist(psi$Sig[i]),nrow=m))
+        for (j in 1:n) {
+          a<-numeric(1)
+          for (w in 1:g) {
+            a<-a+psi$p[w]*dmvnorm(x = x[j,],mean = unlist(psi$mu[w]),sigma = 
+                                    matrix(data = unlist(psi$Sig[w]),nrow = m))
+          }
+          mt[j,i]<-mt[j,i]/a
         }
-        t[j,i]<-t[j,i]/a
       }
+      ent<-numeric(1)
+      for (w in 1:g){
+        ent<-ent+mt[,w]%*%log(mt[,w])
+      }
+      FVerCLC<-(-2)*FVer+2*as.numeric(ent)
+      return(FVerCLC)
     }
-    ent<-numeric(1)
-    for (w in 1:g){
-      ent<-ent+t[,w]%*%log(t[,w])
+    FVerCLC<-(-2)*FVer+2*as.numeric(ent)
+  }
+  if (metodo=="KIC"|metodo=="ALL"){
+    FVerKIC<-(-2)*FVer+3*(length(unlist(psi))+1)
+    if (metodo=="KIC"){
+      return(FVerKIC)  
     }
-    FVer<-(-2)*FVer+2*as.numeric(ent)
-    return(FVer)
-  } else if (metodo=="KIC"){
-    FVer<-sum(log(t))
-    FVer<-(-2)*FVer+3*(length(unlist(psi))+1)
-    return(FVer)
-  } else {
+  } 
+  if (metodo=="ALL"){
+    return(c(AIC=FVerAIC,AWE=FVerAWE,BIC=FVerBIC,CLC=FVerCLC,KIC=FVerKIC))
+  }else {
     print("No se definio correctamente el metodo")
   }
 }
 
 #Ejemplo
-IC(x,g,psi,metodo="AIC")
-IC(x,g,psi,metodo="AWE")
-IC(x,g,psi,metodo="BIC")
-IC(x,g,psi,metodo="CLC")
-IC(x,g,psi,metodo="KIC")
+# IC(x,g,psi,metodo="AIC")
+# IC(x,g,psi,metodo="AWE")
+# IC(x,g,psi,metodo="BIC")
+# IC(x,g,psi,metodo="CLC")
+# IC(x,g,psi,metodo="KIC")
+# IC(x,g,psi,metodo="ALL")
