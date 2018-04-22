dfce90e18a23ab826dbb6e154b10ff863c507630
luisgcalderon <luis.calderon@anahuac.mx>
2018-04-22 10:53:35 -0500

    Avance

--- Algorithms/KMeans Algorithm/KMeans_Algorithm 20180420.R
+++ Algorithms/KMeans Algorithm/KMeans_Algorithm 20180420.R
@@ -24,12 +24,27 @@ fviz_dist(distance, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4
 
 # K-means
 # Inputs: DataSet, and K
+
+KMeansStep<-function(data,k){
+n<-dim(data)[1]
+m<-dim(data)[2]
+p<-numeric(k)
+Mu<-array(data=0,dim = c(m,k))
+Sig<-array(data = 0,dim = c(m^2,k))
+
+KSet <- kmeans(data, centers = k, nstart = 25)
+for (i in 1:k){
+  p[i]<-sum(KSet$cluster==i)/n
+  Mu[,i]<-KSet$centers[i,]
+  x<-scale(data[KSet$cluster==i,],scale = F)
+  Sig[,i]<-as.numeric(as.matrix(t(x))%*%as.matrix(x)/(n-1))
+}
+
+psi<-list(p=p,Mu=Mu,Sig=Sig)
+return(psi)
+}
+x<-faithful  
 k<-2
-KSet <- kmeans(old, centers = k, nstart = 25)
 
-for (i in 1:k)
-x<-old[KSet$cluster==i,]
-as.matrix(t(x))%*%as.matrix(x)/dim(x)[1]
-
-k$centers
-  
+psi<-KMeansStep(data = x,k = k)
+psi
