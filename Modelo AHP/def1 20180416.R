b94f6fd2eeeaa8ad50de4209d4b26e2ca2e27fc8
luisgcalderon <luis.calderon@anahuac.mx>
2018-04-22 14:28:21 -0500

    desarrollo

--- Modelo AHP/def1 20180416.R
+++ Modelo AHP/def1 20180416.R
@@ -3,9 +3,17 @@
 #Analisis de Conglomerados
 #Luis Gerardo Calderon Contreras
 
+#Clear Environment
+rm(list = ls())
 
+#Source
+
+source('~/Anahuac/AnalisisConglomerados/AnalisisConglomerados/Algorithms/KMeans Algorithm/KMeans_Algorithm 20180420.R') #Algoritmos K-Means
+source('~/Anahuac/AnalisisConglomerados/AnalisisConglomerados/Algorithms/EM Algorithm/EM Algorithm MV & Graphs  v1 20180329.R') #Algoritmo EM MV
+source('~/Anahuac/AnalisisConglomerados/AnalisisConglomerados/Criterios de Informacion/Definicion de Funciones.R') #Criterios de Informacion
 #AHP Model Analysis----
 
+x<-faithful
 g<-2 # cantidad de cluster Inicial
 n<-4 # alternativas
 
@@ -16,23 +24,27 @@ n<-4 # alternativas
 ##Step2.1 Modelled with K-Means
 #Calcular los parametros iniciales para modelos de 2,3,4,5 componentes
 #Correr K-MEANS
-
 #Estimar Parametros Iniciales
+psi.c2<-KMeansStep(x,2)
+psi.c3<-KMeansStep(x,3)
+psi.c4<-KMeansStep(x,4)
+psi.c5<-KMeansStep(x,5)
+
 
 ##Step2.2 Modelled with EM
 #Estimar los parametros finales
 
 #Inputs datos,lista de psi, g, graf
 
-psi.2<-EMAlgorithmMV(dato = x,g = 2,psi = psi.2,metodo = "verosi",difmin = 0.001)
-psi.3<-EMAlgorithmMV(dato = x,g = 3,psi = psi.3,metodo = "verosi",difmin = 0.001)
-psi.4<-EMAlgorithmMV(dato = x,g = 4,psi = psi.4,metodo = "verosi",difmin = 0.001)
-psi.5<-EMAlgorithmMV(dato = x,g = 5,psi = psi.5,metodo = "verosi",difmin = 0.001)
+psi.c2<-EMAlgorithmMV(dato = x,g = 2,psi = psi.c2,metodo = "verosi",difmin = 0.001)
+psi.c3<-EMAlgorithmMV(dato = x,g = 3,psi = psi.c3,metodo = "verosi",difmin = 0.001)
+psi.c4<-EMAlgorithmMV(dato = x,g = 4,psi = psi.c4,metodo = "verosi",difmin = 0.001)
+psi.c5<-EMAlgorithmMV(dato = x,g = 5,psi = psi.c5,metodo = "verosi",difmin = 0.001)
 
 #Step3 Criterion Information Validation----
 
-MCriterion<-data.frame(matrix(data = c(IC(x,g,psi,metodo="ALL"),IC(x,g,psi,metodo="ALL"),IC(x,g,psi,metodo="ALL"),IC(x,g,psi,metodo="ALL"))
-                            ,nrow = n,ncol = 5,byrow = T)); colnames(DeMatrix)<-c("AIC","AWE","BIC","CLC","KIC");row.names(DeMatrix)<-as.character(g:(g+n-1))
+MCriterion<-data.frame(matrix(data = c(IC(x,2,psi.c2,metodo="ALL"),IC(x,3,psi.c3,metodo="ALL"),IC(x,4,psi.c4,metodo="ALL"),IC(x,5,psi.c5,metodo="ALL"))
+                            ,nrow = n,ncol = 5,byrow = T)); colnames(MCriterion)<-c("AIC","AWE","BIC","CLC","KIC");row.names(MCriterion)<-as.character(g:(g+n-1))
 
 
 AHPClusterAnalysis<-function(MCriterion){
